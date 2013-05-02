18ac1d3a4b98cf66f9437ae3454ec242214f651d
JudeBake <julien.bacon.1@gmail.com>
2013-05-02 00:05:38 -0400

    Adding the config file of the peripherics of the system

---
+++ Source/System/SystemPeriphConf.h
@@ -0,0 +1,14 @@
+/*
+ * SystemPeriphConf.h
+ *
+ *  Created on: May 2, 2013
+ *      Author: julien
+ */
+
+#ifndef SYSTEMPERIPHCONF_H_
+#define SYSTEMPERIPHCONF_H_
+
+
+
+
+#endif /* SYSTEMPERIPHCONF_H_ */
