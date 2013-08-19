742ae41a16d55a58089636aefe8baece60baaa5f
JudeBake <julien.bacon.1@gmail.com>
2013-08-19 00:29:15 -0400

    adding the include for AsyncSerialPort1

--- Source/System/SystemPeriphConf.h
+++ Source/System/SystemPeriphConf.h
@@ -10,7 +10,7 @@
 
 /*
  * Serial Port Config
- * Comment/uncomment the line to configure the serial peripherics that will be
+ * Comment/uncomment the lines to configure the serial peripherics that will be
  * used in the project.
  */
 #define ASYNC_SERIAL_PORT1
@@ -21,15 +21,36 @@
 #define ASYNC_SERIAL_PORT6
 
 //#define SYNC_SERIAL_PORT1
-#define SYNC_SERIAL_PORT2
-#define SYNC_SERIAL_PORT3
-#define SYNC_SERIAL_PORT6
+//#define SYNC_SERIAL_PORT2
+//#define SYNC_SERIAL_PORT3
+//#define SYNC_SERIAL_PORT6
 
 //...
 
+/*
+ * Building error for over defined peripherals
+ */
 #if defined ASYNC_SERIAL_PORT1 && defined SYNC_SERIAL_PORT1
-#error "Serial Port 1 used as Asynchrone and Synchrone Port."
+#error "Serial port 1 used as asynchronous and synchronous port."
 #endif
 
+#if defined ASYNC_SERIAL_PORT2 && defined SYNC_SERIAL_PORT2
+#error "Serial port 2 used as asynchronous and synchronous port."
+#endif
+
+#if defined ASYNC_SERIAL_PORT3 && defined SYNC_SERIAL_PORT3
+#error "Serial port 3 used as asynchronous and synchronous port."
+#endif
+
+#if defined ASYNC_SERIAL_PORT6 && defined SYNC_SERIAL_PORT6
+#error "Serial port 6 used as asynchronous and synchronous port."
+#endif
+
+/*
+ * Includes of the used peripherals
+ */
+#ifdef ASYNC_SERIAL_PORT1
+#include "AsyncSerialPort1.h"
+#endif
 
 #endif /* SYSTEMPERIPHCONF_H_ */
